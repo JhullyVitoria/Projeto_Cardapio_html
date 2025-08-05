ce0c97651afd0fac373237c0f49e30ad9c7a4f6a
JhullyVitoria <118947407+JhullyVitoria@users.noreply.github.com>
2025-08-05 19:43:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -213,9 +213,9 @@ if (paymentMethod === "dinheiro" && !isNaN(valorPago)) {
 
   if (valorPago > total) {
     const troco = (valorPago - total).toFixed(2);
-    message += `\n *Troco:* R$${troco}`;
+    message += `\n*Troco:* R$${troco}`;
   } else if (valorPago < total) {
-    message += `\n *Valor pago insuficiente!*`;
+    message += `\n*Valor pago insuficiente!*\n`;
   }
 }
 
